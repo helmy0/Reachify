1bc3201a4a685bc74a386a464e91594ed5571c5e
Omar Farrag <omar.tarek.farrag123@gmail.com>
2024-02-17 09:52:14 +0000

    hid keys

--- CRM/settings.py
+++ CRM/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from accounts import keys
+from . import keys
 
 load_dotenv()
 
